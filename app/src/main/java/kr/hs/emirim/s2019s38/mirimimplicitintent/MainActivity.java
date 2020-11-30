a09c6d314400a95e8046ad6e9a015ad2549e60fb
sumin_030829 <sumin0829@kako.com>
2020-11-30 19:09:50 +0900

    Set OncleickListener(Button)

--- app/src/main/java/kr/hs/emirim/s2019s38/mirimimplicitintent/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimimplicitintent/MainActivity.java
@@ -3,6 +3,8 @@ package kr.hs.emirim.s2019s38.mirimimplicitintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCall=findViewById(R.id.btn_call);
+        Button btnWeb=findViewById(R.id.btn_hoom);
+        Button btnMap=findViewById(R.id.btn_map);
+        Button btnSearch=findViewById(R.id.btn_search);
+        Button btnSms=findViewById(R.id.btn_sms);
+        Button btnCamera=findViewById(R.id.btn_camera);
+
+        btnCall.setOnClickListener(btnListener);
+        btnWeb.setOnClickListener(btnListener);
+        btnMap.setOnClickListener(btnListener);
+        btnSearch.setOnClickListener(btnListener);
+        btnSms.setOnClickListener(btnListener);
+        btnCamera.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 }
